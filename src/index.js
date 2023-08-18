18bb7ff9820d7c2c1c8bf3168c0e93dd4f5b4336
EnayetHossain <enayethossasin135@gmail.com>
2023-08-18 18:54:30 +0600

    search bar and category bar

--- src/index.js
+++ src/index.js
@@ -3,5 +3,40 @@ import backgroundImage from "./assets/pxfuel.jpg";
 import "./styles/style.scss";
 
 document.getElementById("logo").setAttribute("src", logo);
-document.querySelector("header").style.backgroundImage = `url(${backgroundImage})`;
-// console.log(document.querySelector("header"))
+const header = document.querySelector("header");
+header.style.backgroundImage = `url(${backgroundImage})`;
+
+const searchBar = document.querySelector(".search-bar");
+const optionItems = [...document.querySelectorAll(".option-item"), searchBar];
+const optionItems2 = [searchBar, ...document.querySelectorAll(".option-item")]
+const closeBtn = document.querySelector(".fa-x");
+const searchBtn = document.querySelector(".search-btn");
+
+searchBtn.addEventListener("click", ()=>{
+    optionItems.forEach((item, index)=>{
+        setTimeout(()=>{
+            if(item.className === "option-item"){
+                item.classList.add("option-item-hide")
+            }else{
+                item.classList.add("search-bar-active")
+            }
+            // console.log(typeof item.className)
+        }, index * 140)
+    });
+
+    closeBtn.style.visibility = "visible"
+});
+
+closeBtn.addEventListener("click", ()=>{
+    optionItems2.forEach((item, index)=>{
+        setTimeout(()=>{
+            if(item.classList.contains("search-bar")){
+                item.classList.remove("search-bar-active");
+            }else{
+                item.classList.remove("option-item-hide");
+            }
+        }, index * 140);
+        // console.log(item.classList.contains("option-item-hide"));
+    });
+    closeBtn.style.visibility = "hidden"
+});
